417020ccd9fa600fc284da755a74abf50354338e
Ze Dang <4649tz@gmail.com>
2019-05-31 19:10:08 +0800

    docs: finish hw03

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
-module.exports = function main() {
+module.exports = function main(seq) {
     console.log("Debug Info");
-    return 'Hello World!';
+    const row1 = '._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.'.split('   ');
+    const row2 = '|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|'.split('   ');
+    const row3 = '|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|'.split('   ');
+    return getNumber(seq, row1) + getNumber(seq, row2) + getNumber(seq, row3);
 };
+
+function getNumber(seq, row) {
+    let ans = [];
+    for (let i = 0; i < seq.length; i++) {
+        const curr = Number(seq[i]);
+        ans.push(row[curr]);
+    }
+    return ans.join(' ') + '\n';
+}
+
